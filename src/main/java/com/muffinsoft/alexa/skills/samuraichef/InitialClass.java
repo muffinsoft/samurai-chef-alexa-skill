b84e8c9a06242753350a084d29a999f71d45b4bd
kulynych_e <kulynychevhenyy@gmail.com>
2018-10-09 23:00:41 +0300

    - initial commit

---
+++ src/main/java/com/muffinsoft/alexa/skills/samuraichef/InitialClass.java
@@ -0,0 +1,8 @@
+package com.muffinsoft.alexa.skills.samuraichef;
+
+public class InitialClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Alexa");
+    }
+}
